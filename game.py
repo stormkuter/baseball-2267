4f6bd255a170511065f0098e29fb08cf84188a53
gunho <qlfydehd@gmail.com>
2024-07-10 14:46:38 +0900

    [feature] 41

--- game.py
+++ game.py
@@ -9,3 +9,8 @@ class Game:
         for number in guessNumber:
             if not ord('0') <= number <= ord('9'):
                 raise TypeError()
+
+        if number[0] == number[1] or \
+            number[0] == number[2] or \
+            number[1] == number[2]:
+            raise TypeError()
