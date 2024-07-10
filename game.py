45b9b82bf660e0aea5a496c24fc3d8c33c6c2f8e
gunho <qlfydehd@gmail.com>
2024-07-10 14:59:38 +0900

    [feature] 47

--- game.py
+++ game.py
@@ -1,6 +1,14 @@
+from game_result import GameResult
+
+
 class Game:
+    def __init__(self):
+        super().__init__()
+        self.question = ""
     def guess(self, guessNumber):
         self.assert_illegal_value(guessNumber)
+        return GameResult(True, 3, 0)
+
 
     def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
@@ -8,7 +16,7 @@ class Game:
         if len(guessNumber) != 3:
             raise TypeError()
         for number in guessNumber:
-            if not ord('0') <= number <= ord('9'):
+            if not ord('0') <= ord(number) <= ord('9'):
                 raise TypeError()
         if self.isDuplicatedNumber(guessNumber):
             raise TypeError()
---
+++ game_result.py
@@ -0,0 +1,5 @@
+class GameResult:
+    def __init__(self, solved, strike, ball):
+        self.solved = solved
+        self.strikes = strike
+        self.balls = ball
